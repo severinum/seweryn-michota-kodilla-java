eaf0750f9c891a4b33e82cc58b1790c0c7addc5d
severinum <sewerynmichota@gmail.com>
2020-08-05 13:54:05 +0100

    Pierwszy commit z Ubuntu

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+
+    public static void main(String[] args) {
+        System.out.println("Modul 6. Wprowadzenie do testowania oprogramowania");
+    }
+}
