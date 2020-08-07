f662a8a5a6d29c14251336e071116bd9259ea936
Sever <sewerynmichota@gmail.com>
2020-08-07 12:34:52 +0100

    Weather Stub - before Mockito

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.weather.stub;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class WeatherForecastTestSuite {
+
+    @Test
+    public void testCalculateForecastWithStub() {
+        // given
+        Temperatures temperatures = new TemperaturesStub();
+        WeatherForecast weatherForecast = new WeatherForecast(temperatures);
+        // when
+        int qtyOfSensors = weatherForecast.calculateForecast().size();
+        // then
+        Assertions.assertEquals(5, qtyOfSensors);
+    }
+}
