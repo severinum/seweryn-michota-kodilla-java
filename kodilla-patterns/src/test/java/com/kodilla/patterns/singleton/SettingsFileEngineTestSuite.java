bc991329c3141bb6f631c1ec006d4ede7dad8449
SeverinuM <sewerynmichota@gmail.com>
2020-10-09 22:33:21 +0100

    ENUM

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/SettingsFileEngineTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SettingsFileEngineTestSuite {
+
+    private static SettingsFileEngine settingsFileEngine;
+
+    @BeforeAll
+    public static void openSettingsFile(){
+        settingsFileEngine = new SettingsFileEngine();
+        settingsFileEngine.open("myapp.settings");
+    }
+
+    @AfterAll
+    public static void closeSettingsFile(){
+        settingsFileEngine.close();
+    }
+
+    @Test
+    void testGetFileName(){
+        // given
+        // when
+        String fileName = settingsFileEngine.getFileName();
+        System.out.println("Opened: " + fileName);
+        // then
+        assertEquals("myapp.settings", fileName);
+    }
+
+    @Test
+    void testLoadSettings(){
+        // given
+        // when
+        boolean result = settingsFileEngine.loadSettings();
+        // then
+        assertTrue(result);
+    }
+
+}
