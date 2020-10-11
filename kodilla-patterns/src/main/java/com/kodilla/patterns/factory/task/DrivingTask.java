be4ca2ea68eb6d62f8f35e933fcc218a47e603f4
SeverinuM <sewerynmichota@gmail.com>
2020-10-11 19:35:04 +0100

    Factory pattern

--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/task/DrivingTask.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/task/DrivingTask.java
@@ -13,7 +13,6 @@ public class DrivingTask implements Task {
         isExecuted = false;
     }
 
-
     @Override
     public void executeTask() {
         isExecuted = true;
--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/task/TaskFactory.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/task/TaskFactory.java
@@ -1,7 +1,22 @@
 package com.kodilla.patterns.factory.task;
 
+import com.kodilla.patterns.factory.Shape;
+
 public class TaskFactory {
     public static final String SHOPPING_TASK = "SHOPPING_TASK";
-    public static final String SHOPPING_TASK = "SHOPPING_TASK";
-    public static final String SHOPPING_TASK = "SHOPPING_TASK";
+    public static final String PAINTING_TASK = "PAINTING_TASK";
+    public static final String DRIVING_TASK = "DRIVING_TASK";
+
+    public final Task createTask(final String taskClass){
+        switch (taskClass){
+            case SHOPPING_TASK:
+                return new ShoppingTask("Shopping Task", "Bread", 2);
+            case PAINTING_TASK:
+                return new PaintingTask("Painting Task", "Warm Yellow", "Kitchen walls");
+            case DRIVING_TASK:
+                return new DrivingTask("Driving Task", "Szczecin", "My Car");
+            default:
+                return null;
+        }
+    }
 }
